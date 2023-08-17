d62e7f092bd11d02a0d1425a4c4b5b197b7bcb71
JayantBishnoi <73429598+JayantBishnoi@users.noreply.github.com>
2023-08-17 12:37:54 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+attendees = list(attendees)
+# # present = 
+# dict1 = {'Name': attendees, 'Attendance': 'Present'}
+# dframe = pd.DataFrame(dict1) 
+# today = date.today()
+# str1 = str(today) + '.csv'
+# dframe.to_csv(str1)
